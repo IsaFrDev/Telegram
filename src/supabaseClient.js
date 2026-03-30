d6c7178d3270bfea50dae02af8be97354499d2e8
Islombek <mansurovislombek130@gmail.com>
2026-03-30 12:31:31 +0500

    chore: update supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://vkckxborcohmovtogsrn.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrY2t4Ym9yY29obW92dG9nc3JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NjAwOTgsImV4cCI6MjA4OTQzNjA5OH0.EUhVHt76SqDRmzBNy7sRCujewUQ6mHi6EVlRHFz7dbU';
+const SUPABASE_URL = 'https://kxyzzkylkutjbiyeluck.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4eXp6a3lsa3V0amJpeWVsdWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NDc4OTYsImV4cCI6MjA5MDQyMzg5Nn0.Y814FhhsN-CIxutJ90LvhkxoWnG6BlaZxRpqFoA8oM4';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
